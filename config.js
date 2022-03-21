e916f60386d97efab150f91eef3e5884f6872233
karankhadke222 <77437951+karankhadke222@users.noreply.github.com>
2022-03-21 12:28:48 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,9 +1,16 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyDdTGfIcj2b3SFHbaJcGBoGrbNL3PEBrto",
+    authDomain: "voting-app-4bcbe.firebaseapp.com",
+    databaseURL: "https://voting-app-4bcbe-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "voting-app-4bcbe",
+    storageBucket: "voting-app-4bcbe.appspot.com",
+    messagingSenderId: "82442156364",
+    appId: "1:82442156364:web:eaf4e675cc0daa8ed5b7e9"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
